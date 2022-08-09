7f6f1114e055254a60d4002d4f114198f848bc66
David <electromandarin@gmail.com>
2022-08-09 13:54:48 +0300

    removed old porfolio proj, added new tech

--- FirstLast-test-copy/src/main.js
+++
@@ -1,9 +0,0 @@
-const text = document.querySelector('.main-middle_center__text');
-
-window.addEventListener('mousemove', (e)=>{
-    let x = e.clientX / window.innerWidth;
-    let y = e.clientY / window.innerHeight;
-
-    text.style.transform = 'translateY(' + y * 50 + 'px) translateX('+ x * 20 + 'px)';
-    text.style.transition = 'transform ease';
-});
